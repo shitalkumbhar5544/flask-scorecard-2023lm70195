c4f9463c3332025b152d39ef2db43accc09fc0c3
Shital kumbhar <2023lm70195@wilp.bits-pilani.ac.in>
2025-08-17 20:56:35 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,12 @@
+import pytest
+from app import app
 
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
